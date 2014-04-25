5231601d1e5ff20e7a8361bb1daecca7a96953f5
asenovm <asenov.m@gmail.com>
2014-04-26 00:12:09 +0300

    * Added mock services

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-The server part of the edu-video project
+# edu-video-server [![Build Status](https://secure.travis-ci.org/asenovm/edu-video-server.png?branch=master)](http://travis-ci.org/asenovm/edu-video-server)
+
+The best module ever.
+
+## Getting Started
+Install the module with: `npm install edu-video-server`
+
+```javascript
+var edu-video-server = require('edu-video-server');
+edu-video-server.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2014 Martin Asenov. Licensed under the MIT license.
---
+++ test/edu-video-server_test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var eduVideoServer = require('../lib/edu-video-server.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports.eduVideoServer = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.equal(eduVideoServer.awesome(), 'awesome', 'should be awesome.');
+    test.done();
+  }
+};
